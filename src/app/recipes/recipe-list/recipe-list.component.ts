94a45ef973f69652f6e6ab4f18ff55f19222f23c
Subash_Chandra <s.chandra106@gmail.com>
2023-11-01 04:46:16 +0530

    Added recipe list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Aaloo do pyaja',
+      'One of the Indian delicious recipe',
+      '../../assets/aloo-do-pyaza-inside_2.jpg'
+    ),
+    new Recipe(
+      'Tomato Rasam',
+      'tomato-rasam south indian recipe',
+      '../../assets/tomato-rasam.jpg'
+    ),
+
+    new Recipe(
+      'Butter chicken',
+      'Butter-chicken.webp',
+      '../../assets/Butter-chicken.webp'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
