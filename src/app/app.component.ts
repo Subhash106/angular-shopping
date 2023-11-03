0cc2aa69460ecf594f4e27b5960bb2924662fb2f
Subash_Chandra <s.chandra106@gmail.com>
2023-11-03 06:18:20 +0530

    worked on recipe details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  showRecipe: boolean = true;
+
+  onMenuItemClick(menuItem: string) {
+    if (menuItem === 'recipe') {
+      this.showRecipe = true;
+    } else {
+      this.showRecipe = false;
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export default class HeaderComponent {}
+export default class HeaderComponent {
+  @Output() menuItemSelected = new EventEmitter<string>();
+
+  menuClickHandler(e: MouseEvent, menuItem: string) {
+    e.preventDefault();
+
+    this.menuItemSelected.emit(menuItem);
+  }
+}
