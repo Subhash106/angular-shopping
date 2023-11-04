aebe61f4100dc70cf1b30de1cb544c5af05b6dcb
Subash_Chandra <s.chandra106@gmail.com>
2023-11-04 05:36:37 +0530

    Added recipe functionality

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('ingredientName') ingredientName: ElementRef;
+  @ViewChild('ingredientAmount') ingredientAmount: ElementRef;
 
-  constructor() { }
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addIngredientHandler() {
+    this.ingredientAdded.emit(
+      new Ingredient(
+        this.ingredientName.nativeElement.value,
+        this.ingredientAmount.nativeElement.value
+      )
+    );
   }
-
 }
